d274621835a4fe5fcf2e98e41c90296ed5727aa2
BilalTheOne <bilalyassine1023@gmail.com>
2023-03-17 16:38:35 -0700

    reversed

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -4,9 +4,11 @@
  * followed by a new line
  * Return: Always 0 (Success)
  */
-int main() {
+int main(void)
+{
 int i;
-for(i = 122; i >= 97; i--) {
+for (i = 122; i >= 97; i--)
+{
 putchar(i);
 }
 putchar('\n');
