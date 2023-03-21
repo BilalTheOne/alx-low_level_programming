a4fa12cd619c8c504a2d266ea89e17b9fd3547b1
BilalTheOne <bilalyassine1023@gmail.com>
2023-03-21 14:43:40 -0700

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
-/*
- * A code that prints _putchar and add a new line'
+/**
+ * main - main function
+ * _putchar: print the alphabet putchar
+ * Return: always 0
  */
 int main(void)
-{
-	/*
-	 * function of printing
-	 */
-	printf("_putchar\n");
+}
+	_putchar('_');
+	_putchar(p);
+	_putchar(u);
+	_putchar(t);
+	_putchar(c);
+	_putchar(h);
+	_putchar(a);
+	_putchar(r);
+	_putchar('\n');
 	return (0);
 }
